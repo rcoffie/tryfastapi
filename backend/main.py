81ab4859517778ff0909bc79cd1f6ff6add8faa1
Robert Coffie <rcoffie22@yahoo.com>
2021-09-08 14:37:39 +0000

    first fastapi commit

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI 
+from core.config import settings
+from db.session import engine 
+from db.base import Base
+
+
+
+
+
+def create_tables():
+  Base.metadata.create_all(bind=engine)
+
+def start_application():
+
+  app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+  create_tables()
+  return app 
+
+app = start_application()
+
+# app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+
+
+
+
+@app.get('/')
+def hello_api():
+  return {'detail':'hello world'}
